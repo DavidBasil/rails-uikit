60d945bde123bc2937447c668d2e2c32303c6a13
David Basil <info@basildavid.com>
2018-02-24 17:09:40 +0400

    Add buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'button/index'
+
 	resources :accordion
 	resources :alert
 	resources :align
 	resources :animation
 	resources :article
+	resources :background
+	resources :button
 end
