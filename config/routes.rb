3071be07fca11575e86270223c23ece84e97f7b1
David Basil <info@basildavid.com>
2018-02-24 18:07:17 +0400

    Add container

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'container/index'
+
   get 'comment/index'
 
   get 'column/index'
@@ -20,4 +22,5 @@ Rails.application.routes.draw do
 	resources :close
 	resources :column
 	resources :comment
+	resources :container
 end
